4f591453ec809e7b420c5a167ac9ecb64c7d6fae
Naveen <naveenkumar51@gmail.com>
2013-11-28 12:36:15 +0530

    assets compiled

--- db/migrate/20131124211025_add_banner_to_event.rb
+++ db/migrate/20131124211025_add_banner_to_event.rb
@@ -1,8 +1,8 @@
 class AddBannerToEvent < ActiveRecord::Migration
   def change
-    add_column :refinery_events , :published, :boolean, :default => 0
-    add_column :refinery_events , :pay_with_card, :boolean, :default => 0
-    add_column :refinery_events , :pay_with_cash, :boolean, :default => 0
-    add_column :refinery_events , :pay_with_check, :boolean, :default => 0
+    add_column :refinery_events , :published, :boolean, :default => true
+    add_column :refinery_events , :pay_with_card, :boolean, :default => true
+    add_column :refinery_events , :pay_with_cash, :boolean, :default => true
+    add_column :refinery_events , :pay_with_check, :boolean, :default => true
   end
 end
