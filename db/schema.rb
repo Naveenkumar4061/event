7c5c261d47ad17f175a7384b3517d270ddb955b5
Naveen <naveenkumar51@gmail.com>
2013-11-30 16:49:21 +0530

    bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -47,10 +47,10 @@ ActiveRecord::Schema.define(:version => 20131129165951) do
     t.integer  "updated_by"
     t.text     "schedule"
     t.text     "terms_conditions"
-    t.boolean  "published",            :default => false
-    t.boolean  "pay_with_card",        :default => false
-    t.boolean  "pay_with_cash",        :default => false
-    t.boolean  "pay_with_check",       :default => false
+    t.boolean  "published",            :default => true
+    t.boolean  "pay_with_card",        :default => true
+    t.boolean  "pay_with_cash",        :default => true
+    t.boolean  "pay_with_check",       :default => true
     t.string   "banner_file_name"
     t.string   "banner_content_type"
     t.integer  "banner_file_size"
@@ -144,22 +144,22 @@ ActiveRecord::Schema.define(:version => 20131129165951) do
     t.string   "outlet"
     t.string   "address"
     t.string   "timing"
-    t.string   "event_id"
+    t.integer  "event_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
   create_table "refinery_events_registrations", :force => true do |t|
     t.integer  "no_of_tickets"
-    t.decimal  "total",         :precision => 10, :scale => 0
-    t.decimal  "discount",      :precision => 10, :scale => 0
-    t.decimal  "sales_tax",     :precision => 10, :scale => 0
-    t.decimal  "adjustment",    :precision => 10, :scale => 0
+    t.decimal  "total"
+    t.decimal  "discount"
+    t.decimal  "sales_tax"
+    t.decimal  "adjustment"
     t.integer  "event_id"
     t.integer  "user_id"
     t.string   "state"
-    t.datetime "created_at",                                   :null => false
-    t.datetime "updated_at",                                   :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
   create_table "refinery_events_tickets", :force => true do |t|
@@ -167,11 +167,11 @@ ActiveRecord::Schema.define(:version => 20131129165951) do
     t.string   "description"
     t.datetime "sale_start_on"
     t.datetime "sale_end_on"
-    t.decimal  "original_price",       :precision => 10, :scale => 0
-    t.decimal  "discounted_price",     :precision => 10, :scale => 0
-    t.decimal  "commission",           :precision => 10, :scale => 0
+    t.decimal  "original_price"
+    t.decimal  "discounted_price"
+    t.decimal  "commission"
     t.boolean  "is_service_tax"
-    t.decimal  "service_tax_percent",  :precision => 10, :scale => 0
+    t.decimal  "service_tax_percent"
     t.integer  "max_quantity"
     t.integer  "min_quantity"
     t.integer  "max_quantity_on_sale"
@@ -182,8 +182,8 @@ ActiveRecord::Schema.define(:version => 20131129165951) do
     t.boolean  "is_not_display"
     t.integer  "tickets_sold"
     t.integer  "event_id"
-    t.datetime "created_at",                                          :null => false
-    t.datetime "updated_at",                                          :null => false
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
   end
 
   create_table "refinery_events_venues", :force => true do |t|
